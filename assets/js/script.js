dc45a57f22f2f025847f10e487300a850a217975
Benjamin Turpin <lhp6.bturpin@gmail.com>
2021-05-07 15:07:13 +0200

    ajout des derniere fonction js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const myWorld = [
+const myWord = [
     "BONJOUR",
     "BONSOIR",
     "AUREVOIR"
@@ -6,24 +6,33 @@ const myWorld = [
 
 const allButton = document.querySelectorAll("button[data-letter]");
 
-test.addEventListener("click", function () {
-    const selectDiv = document.querySelectorAll("div[data-letterguest]");
-    console.log(selectDiv.length)
-    if (selectDiv.length < 1) {
-        let wordRandom = myWorld[Math.floor(Math.random() * 3)];
-        for (i = 0; i < wordRandom.length; i++) {
-            console.log(wordRandom[i]);
-            let createDiv = document.createElement("div");
-            createDiv.setAttribute("data-letterguest", wordRandom[i]);
-            createDiv.innerHTML = wordRandom[i];
-            document.body.appendChild(createDiv);
-        }
-    } else {
-        location.reload();
+window.addEventListener("DOMContentLoaded", function () {
+    let wordRandom = myWord[Math.floor(Math.random() * 3)];
+    for (i = 0; i < wordRandom.length; i++) {
+        let createDiv = document.createElement("div");
+        createDiv.setAttribute("data-letterguest", wordRandom[i]);
+        createDiv.setAttribute("class", "invisible");
+        createDiv.innerHTML = wordRandom[i];
+        document.body.appendChild(createDiv);
     }
 });
 
-
+buttonReset.addEventListener("click", function () {
+    const selectDiv = document.querySelectorAll("div");
+    if (selectDiv.length > 1) {
+        for (i = 0; i < selectDiv.length; i++) {
+            selectDiv[i].remove();
+        }
+        let wordRandom = myWord[Math.floor(Math.random() * 3)];
+        for (i = 0; i < wordRandom.length; i++) {
+            let createDiv = document.createElement("div");
+            createDiv.setAttribute("data-letterguest", wordRandom[i]);
+            createDiv.setAttribute("class", "invisible");
+            createDiv.innerHTML = wordRandom[i];
+            document.body.appendChild(createDiv);
+        }
+    }
+});
 
 allButton.forEach(element => {
     element.addEventListener("click", function () {
@@ -31,20 +40,21 @@ allButton.forEach(element => {
         this.disabled = true;
         for (i = 0; i < allDiv.length; i++) {
             if (this.dataset.letter == allDiv[i].dataset.letterguest) {
-                allDiv[i].style.color = "red";
+                allDiv[i].className = "visible";
                 this.value = 1;
-            } else {
-                console.log("Erreur");
             }
         }
-        if (this.value == 1){
+        if (this.value == 1) {
             this.style.background = "green";
-            this.style.color = "white"
+            this.style.color = "white";
         } else {
             this.style.background = "red";
-            this.style.color = "white"
-            if(life.value == 0){
+            this.style.color = "white";
+            if (life.value == 1) {
                 life.value = "Vous etes mort";
+                for (i = 0; i < allButton.length; i++) {
+                    allButton[i].disabled = true;
+                }
             } else {
                 life.value--;
             }
