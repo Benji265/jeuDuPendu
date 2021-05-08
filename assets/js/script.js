8e394e074337a747c048c1c211b77a41a8bdb346
Benjamin Turpin <lhp6.bturpin@gmail.com>
2021-05-08 15:09:23 +0200

    Mise en forme de l'html

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,36 +12,39 @@ window.addEventListener("DOMContentLoaded", function () {
         let createDiv = document.createElement("div");
         createDiv.setAttribute("data-letterguest", wordRandom[i]);
         createDiv.setAttribute("class", "invisible");
-        createDiv.innerHTML = wordRandom[i];
-        document.body.appendChild(createDiv);
+        createDiv.innerHTML = "_";
+        wordGuest.appendChild(createDiv);
     }
 });
 
 buttonReset.addEventListener("click", function () {
-    const selectDiv = document.querySelectorAll("div");
+    const selectDiv = document.querySelectorAll("div[data-letterguest]");
     if (selectDiv.length > 1) {
-        for (i = 0; i < selectDiv.length; i++) {
-            selectDiv[i].remove();
-        }
-        let wordRandom = myWord[Math.floor(Math.random() * 3)];
-        for (i = 0; i < wordRandom.length; i++) {
-            let createDiv = document.createElement("div");
-            createDiv.setAttribute("data-letterguest", wordRandom[i]);
-            createDiv.setAttribute("class", "invisible");
-            createDiv.innerHTML = wordRandom[i];
-            document.body.appendChild(createDiv);
-        }
+        location.reload();
+        //     for (i = 0; i < selectDiv.length; i++) {
+        //         selectDiv[i].remove();
     }
+    //     let wordRandom = myWord[Math.floor(Math.random() * 3)];
+    //     for (i = 0; i < wordRandom.length; i++) {
+    //         let createDiv = document.createElement("div");
+    //         createDiv.setAttribute("data-letterguest", wordRandom[i]);
+    //         createDiv.setAttribute("class", "invisible");
+    //         createDiv.innerHTML = wordRandom[i];
+    //         wordGuest.appendChild(createDiv);
+    //     }
+    // }
 });
 
 allButton.forEach(element => {
     element.addEventListener("click", function () {
-        const allDiv = document.querySelectorAll("div");
+        const allDiv = document.querySelectorAll("div[data-letterguest]");
         this.disabled = true;
         for (i = 0; i < allDiv.length; i++) {
             if (this.dataset.letter == allDiv[i].dataset.letterguest) {
                 allDiv[i].className = "visible";
+                allDiv[i].innerHTML = this.dataset.letter;
                 this.value = 1;
+                console.log("toto")
             }
         }
         if (this.value == 1) {
@@ -58,7 +61,6 @@ allButton.forEach(element => {
             } else {
                 life.value--;
             }
-
         }
     });
 });
